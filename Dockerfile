ed0f3c875bc3788a5e77326d5da38455ef80ea60
Jake Teton-Landis <just.1.jake@gmail.com>
2019-04-24 15:07:32 -0700

    init

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM node:10.15.3-slim
+
+RUN mkdir /out
+
+# libx11, etc: Chromium dependencies
+RUN apt-get update && \
+  apt-get -y upgrade && \
+  apt-get install -y \
+    libx11-6 libxcomposite1 libx11-xcb1 libx11-dev libgtk-3-0 libnss3 libxss1 libsoundio1 \
+  && \
+  apt-get autoclean && \
+  apt-get clean && \
+  rm -rf /var/lib/apt/lists/*
+
+# Install Noto Color Emoji font so our Chromium can render color emoji like a typical end-user machine.
+RUN curl --fail -s -S https://s3-us-west-2.amazonaws.com/notion-sources/fonts-noto-color-emoji_0~20180810-1_all.deb \
+      -o /tmp/fonts-noto-color-emoji.deb && \
+      dpkg -i /tmp/fonts-noto-color-emoji.deb && \
+      rm /tmp/fonts-noto-color-emoji.deb
+
+# Install puppeteer; which auto-downloads Chromium
+COPY package.json /app/package.json
+COPY package-lock.json /app/package-lock.json
+RUN cd /app && npm ci
+
+# Copy rest of example code
+COPY . /app
---
+++ example.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const puppeteer = require('puppeteer')
+
+const htmlContent = `
+<html>
+<head>
+  <style>
+    html {
+      font-family: sans-serif, "Noto Color Emoji"
+    }
+  </style>
+</head>
+<body>
+
+😀 😁 😂 🤣 😃 😄 😅 😆 😉 😊 😋 😎 😍 😘 🥰 😗 😙 😚 ☺️ 🙂 🤗 🤩 🤔 🤨 😐 😑 😶 🙄 😏 😣 😥 😮 🤐 😯 😪 😫 😴 😌 😛 😜 😝 🤤 😒 😓 😔 😕 🙃 🤑 😲 ☹️ 🙁 😖 😞 😟 😤 😢 😭 😦 😧 😨 😩 🤯 😬 😰 😱 🥵 🥶 😳 🤪 😵 😡 😠 🤬 😷 🤒 🤕 🤢 🤮 🤧 😇 🤠 🤡 🥳 🥴 🥺 🤥 🤫 🤭 🧐 🤓 😈 👿 👹 👺 💀 👻 👽 🤖 💩 😺 😸 😹 😻 😼 😽 🙀 😿 😾
+
+🐶 🐱 🐭 🐹 🐰 🦊 🦝 🐻 🐼 🦘 🦡 🐨 🐯 🦁 🐮 🐷 🐽 🐸 🐵 🙈 🙉 🙊 🐒 🐔 🐧 🐦 🐤 🐣 🐥 🦆 🦢 🦅 🦉 🦚 🦜 🦇 🐺 🐗 🐴 🦄 🐝 🐛 🦋 🐌 🐚 🐞 🐜 🦗 🕷 🕸 🦂 🦟 🦠 🐢 🐍 🦎 🦖 🦕 🐙 🦑 🦐 🦀 🐡 🐠 🐟 🐬 🐳 🐋 🦈 🐊 🐅 🐆 🦓 🦍 🐘 🦏 🦛 🐪 🐫 🦙 🦒 🐃 🐂 🐄 🐎 🐖 🐏 🐑 🐐 🦌 🐕 🐩 🐈 🐓 🦃 🕊 🐇 🐁 🐀 🐿 🦔 🐾 🐉 🐲 🌵 🎄 🌲 🌳 🌴 🌱 🌿 ☘️ 🍀 🎍 🎋 🍃 🍂 🍁 🍄 🌾 💐 🌷 🌹 🥀 🌺 🌸 🌼 🌻 🌞 🌝 🌛 🌜 🌚 🌕 🌖 🌗 🌘 🌑 🌒 🌓 🌔 🌙 🌎 🌍 🌏 💫 ⭐️ 🌟 ✨ ⚡️ ☄️ 💥 🔥 🌪 🌈 ☀️ 🌤 ⛅️ 🌥 ☁️ 🌦 🌧 ⛈ 🌩 🌨 ❄️ ☃️ ⛄️ 🌬 💨 💧 💦 ☔️ ☂️ 🌊 🌫
+
+</body>
+`
+
+const outPath = '/out/out.pdf'
+
+async function main() {
+  const browser = await puppeteer.launch({
+    // Puppeteer will fail otherwise because of IPC issues related
+    // to the large PDF.
+    // See https://github.com/GoogleChrome/puppeteer/issues/2735
+    pipe: true,
+    args: ['--no-sandbox'],
+  })
+  console.log('Puppeteer launched successfully')
+  console.log(browser.version)
+
+  const page = await browser.newPage()
+  console.log('Sending HTML content to browser:')
+  console.log(htmlContent)
+  await page.setContent(htmlContent)
+
+  console.log(`Writing PDF to: ${outPath}`)
+  await page.pdf({
+    path: outPath,
+  })
+  console.log('Done.')
+}
+
+main()
